9371152cbd6328e8f1c18237781485064fa5ebde
unknown <pfefferberhorion@gmail.com>
2020-10-15 19:56:51 +0300

    done Tank

---
+++ Tank/Tank/WorldOfTanks/Tank.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Tanks.WorldOfTanks
+{
+    public class Tank
+    {
+        public string name { get; private set; }
+        public int armor { get; private set; }
+        public int manevr { get; private set; }
+        public int boekomleke { get; private set; }
+        Random random = new Random();
+        public Tank(string name)
+        {
+            this.name = name;
+            armor = random.Next(1, 100);
+            manevr = random.Next(1, 100);
+            boekomleke = random.Next(1, 100);
+        }
+        public static string operator ^(Tank tank1, Tank tank2)
+        { 
+            int victory1=0;
+            int victory2=0;
+            if (tank1.armor > tank2.armor)
+                victory1++;
+            else
+                victory2++;
+            if (tank1.boekomleke>tank2.boekomleke)
+                victory1++;
+            else
+                victory2++;
+            if (tank1.manevr>tank2.manevr)
+                victory1++;
+            else
+                victory2++;
+            if (victory1 > victory2)
+                return tank1.name + "  VICTORY";
+            else
+                return tank2.name + "  VICTORY";
+        }
+    }
+}
---
+++ Tank/TanksWar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Tanks.WorldOfTanks;
+
+namespace TanksWar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tank> t34 = new List<Tank>();
+            Full(t34, "T - 34");
+
+            List<Tank> tpantera = new List<Tank>();
+            Full(tpantera, "Pantera");
+            for (int i = 0; i < t34.Count; i++)
+                Console.WriteLine(t34[i] ^ tpantera[i]);
+
+        }
+        static void Full(List<Tank> tanks, string name)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(50);
+                tanks.Add(new Tank($"{name}({i+1})"));
+            }
+        }
+
+    }
+}
